063293acc0f789c59573d757d423e464d13bb47e
bkhonye@gmail.com <bkhonye@gmail.com>
2025-12-18 17:15:09 +0200

    feat: add auth APIS(REGISTER,LOGIN,LOGOUT)

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,43 @@
+import mongoose, { Schema } from "mongoose"
+import bcrypt from "bcrypt"
+
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+            minlength: 1,
+            maxlength: 30
+        },
+      
+        password: {
+            type: String,
+            required: true,
+            minlength: 6
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true
+        }
+    },
+    { timestamps: true }
+)
+
+// used for hashing password before saving user 
+userSchema.pre("save", async function () {
+    if (!this.isModified("password")) return;
+    this.password = await bcrypt.hash(this.password, 10);
+});
+
+// method to compare passwords
+userSchema.methods.comparePassword = async function (password) {
+    return await bcrypt.compare(password, this.password);
+};
+export const User = mongoose.model("User", userSchema);
